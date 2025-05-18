e0a76d69f6efbde48def7e1b2e70b683776e7722
Bauti <bautistamendibe10@gmail.com>
2025-05-18 13:05:49 -0300

    onboarding listo

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -22,32 +22,23 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             children: [
               OnboardingPage(
                 title: "Gasta tu tiempo como dinero",
-                subtitle: "Cada día tenés un presupuesto de $2 dólares virtuales. Usar tu celular cuesta: cada hora equivale a $1 dólar. ¡Elegí cómo lo gastás!",
+                subtitle:
+                    "Cada día tenés un presupuesto de \$2 dólares virtuales. Usar tu celular cuesta: cada hora equivale a \$1 dólar. ¡Elegí cómo lo gastás!",
                 image: "assets/imgs/onboarding/onboarding_1.png",
               ),
               OnboardingPage(
                 title: "Cada app tiene su propio presupuesto",
-                subtitle: "Instagram, TikTok, Netflix… asigná cuánto estás dispuesto a “gastar” por cada una. Controlá el uso y evitá los excesos sin darte cuenta.",
+                subtitle:
+                    "Instagram, TikTok, Netflix… asigná cuánto estás dispuesto a “gastar” por cada una. Controlá el uso y evitá los excesos sin darte cuenta.",
                 image: "assets/imgs/onboarding/onboarding_2.png",
               ),
               OnboardingPage(
                 title: "Tomá el control de tu atención",
-                subtitle: "Visualizá en tiempo real cuánto consumiste, qué apps usás más y decidí si querés comprar más tiempo o dejarlo ahí. Tu atención, tu decisión.",
+                subtitle:
+                    "El único método comprobado para dejar de usar tanto el celular. Tu atención, tu libertadf.",
                 image: "assets/imgs/onboarding/onboarding_3.png",
               ),
             ],
-          ),
-          Positioned(
-            bottom: 10,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: SmoothPageIndicator(
-                controller: _controller,
-                count: 3,
-                effect: WormEffect(dotHeight: 10, dotWidth: 10),
-              ),
-            ),
           ),
           Positioned(
             bottom: 50,
@@ -55,7 +46,12 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             right: 20,
             child: Column(
               children: [
-                // Botón blanco (Crear cuenta)
+                SmoothPageIndicator(
+                  controller: _controller,
+                  count: 3,
+                  effect: WormEffect(dotHeight: 10, dotWidth: 10,dotColor: Color.fromARGB(255, 217, 217, 217), activeDotColor: Colors.indigoAccent),
+                ),
+                const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () async {
                     final prefs = await SharedPreferences.getInstance();
@@ -74,20 +70,18 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   child: const Text("Crear una cuenta"),
                 ),
                 const SizedBox(height: 10),
-                // Botón negro (Iniciar sesión)
                 ElevatedButton(
                   onPressed: () async {
                     final prefs = await SharedPreferences.getInstance();
                     await prefs.setBool('seenOnboarding', true);
                     if (!context.mounted) return;
-
                     Navigator.pushNamed(context, '/login');
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
+                    backgroundColor: Colors.indigoAccent,
                     foregroundColor: Colors.white,
                     elevation: 4,
-                    shadowColor: Colors.black26,
+                    shadowColor: Colors.indigoAccent,
                     shape: const StadiumBorder(),
                     minimumSize: const Size(double.infinity, 50),
                   ),
@@ -95,7 +89,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
@@ -118,17 +112,19 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(24),
+      color: Colors.white,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(image, height: 320),
+          Image.asset(image, height: 300),
           SizedBox(height: 20),
-          Text(title.toUpperCase(),
+          Text(title,
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
           SizedBox(height: 16),
           Text(subtitle,
-              textAlign: TextAlign.center, style: TextStyle(fontSize: 16)),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14, color: Colors.grey)),
         ],
       ),
     );
